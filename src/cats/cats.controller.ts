418feab33488f589198cf3c38f864536e9f0cdef
wind-of <adequation@mail.ru>
2020-01-01 10:02:00 +0300

    new: simple handling /cats and  /cats/:id routes

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { CatsService } from './cats.service';
 
 @Controller('cats')
-export class CatsController {}
+export class CatsController {
+  constructor(private readonly catsService: CatsService) {}
+
+  @Get()
+  findALl(): String {
+    return this.catsService.findAll()
+  }
+
+  @Get(':id')
+  findOne(@Param('id') params): String {
+    return this.catsService.findOne(params.id)
+  }
+}
--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class Cats {}
+export class CatsService {
+  
+  findAll(): String {
+    return "You would get all the cats, but they are not."
+  }
+
+  findOne(id: Number): String {
+    return `You would get the cat by id ${id}, but no`
+  }
+}
