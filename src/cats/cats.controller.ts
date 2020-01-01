c9bb8cb1536db4196feb2da6d7b0f62f2d2f8de4
wind-of <adequation@mail.ru>
2020-01-01 13:01:28 +0300

    Added handle of POST-request, fixed `findOne`-method

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,17 +1,24 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body } from '@nestjs/common';
 import { CatsService } from './cats.service';
+import { Cat } from './interfaces/cat.interface'
+import { CreateCatDto } from './dto/create-cat.dto'
 
 @Controller('cats')
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
+  @Post()
+  createCat(@Body() createCatDto: CreateCatDto) {
+    return this.catsService.createCat(createCatDto);
+  }
+
   @Get()
-  findALl(): String {
+  findALl(): Cat[] {
     return this.catsService.findAll()
   }
 
   @Get(':id')
-  findOne(@Param('id') params): String {
+  findOne(@Param() params): Cat | string {
     return this.catsService.findOne(params.id)
   }
 }
