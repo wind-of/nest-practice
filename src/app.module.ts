ff326b23d5b058cb2f41ab318ebf64380e8c9ea6
wind-of <adequation@mail.ru>
2020-01-01 09:59:14 +0300

    fix: moved cats' provider from app.module to cats.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
-import { Cats } from './cats/cats.service';
 
 @Module({
   imports: [CatsModule],
   controllers: [AppController],
-  providers: [AppService, Cats],
+  providers: [AppService],
 })
 export class AppModule {}
