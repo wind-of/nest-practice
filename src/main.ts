3912850ca4fbd51e9bc0afc05144f673efd86215
wind-of <adequation@mail.ru>
2020-01-02 09:56:01 +0300

    note about using middleware

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // If you want to bind middleware for every registered route, you can use the app.use():
+  // app.use(NameOfMiddleWare);
   await app.listen(3000);
 }
 bootstrap();
