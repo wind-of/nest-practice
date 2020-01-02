b8165179a3394c63e07641fcfcf0632f7f695f8f
wind-of <adequation@mail.ru>
2020-01-02 21:01:07 +0300

    made notes about filter registration methods

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,17 @@ async function bootstrap() {
   // If you want to bind middleware for every 
   // registered route, you can use the app.use():
   // app.use(NameOfMiddleWare);
+
+
+  // If you want register the filter globally 
+  // for every route handler
+  // app.useGlobalFilters(new HttpExceptionFilter());
+
+  // Another way to register a filter globally can be seen in the file app.module.ts
+  // The difference between these two methods is that
+  // in the first approach (see above), the filter cannot inject dependencies
+  // (And using the second approach, the filter can)
+
   await app.listen(3000);
 }
 bootstrap();
