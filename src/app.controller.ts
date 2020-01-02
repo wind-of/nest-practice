21e05d3359a5edd6ef762d8729ab8354ba264342
wind-of <adequation@mail.ru>
2020-01-02 20:52:46 +0300

    added notes about the registration of the filter

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, UseFilters, HttpException } from '@nestjs/common';
 import { AppService } from './app.service';
+import { CustomException } from './exceptions/custom.exception';
+import { HttpExceptionFilter } from './filters/http-exception.filter';
 
-@Controller()
+@Controller() // means that the controller that uses this decorator is the main
+
+// You can register filter before exporting controllers and it will be controller-scoped
+// @UseFilters(HttpExceptionFilter) 
+
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): String {
     return this.appService.getHello();
+  }
+
+  @Post()
+
+  // when you set up filter here, it will be method-scoped and applied only to the testException() 
+  @UseFilters(HttpExceptionFilter)
+  // applaying filter by using a class 
+  // instead of an instance (new HttpExceptionFilter())
+  // (preferably, because it reduces memory usage)
+  async testException() {
+    throw new CustomException()
   }
 }
