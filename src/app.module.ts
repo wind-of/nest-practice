bc799ffc31751ba4e14c5be950b6784546dd0140
wind-of <adequation@mail.ru>
2020-01-02 09:57:41 +0300

    bound the CatsMiddleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,27 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
+import { CatsMiddleware } from './middleware/cats.middleware'
 
 @Module({
   imports: [CatsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+
+
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(CatsMiddleware)
+
+      // .exclude({path: cats, method: ...})
+      // You can also expclude an entire controller, like...
+      // .exclude(CatsController)
+
+      .forRoutes({path: 'cats', method: RequestMethod.GET})
+      // ...or define middleware for all the routes defined in the controller
+      // .forRoutes(CatsController)
+  }
+}
