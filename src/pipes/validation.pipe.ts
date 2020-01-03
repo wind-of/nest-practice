77b48dd18476564609abe1cea5ae0b25ee280b72
wind-of <adequation@mail.ru>
2020-01-03 17:00:07 +0300

    set the correct type to the `schema` argument

--- src/pipes/validation.pipe.ts
+++ src/pipes/validation.pipe.ts
@@ -1,10 +1,11 @@
-import { PipeTransform, Injectable, ArgumentMetadata, BadRequestException } from '@nestjs/common';
+import { PipeTransform, Injectable, ArgumentMetadata, BadRequestException } from '@nestjs/common'
+import { Schema } from 'hapi__joi'
 
 export class JoiValidationPipe implements PipeTransform{
-  constructor(private readonly schema: Object) {}
+  constructor(private readonly schema: Schema) {}
 
   transform(value: any, metadata: ArgumentMetadata) {
-    const { error } = this.schema.validate(value)
+    const { error } = this.schema.validate(value);
 
     if(error) {
       throw new BadRequestException('Validation error');
