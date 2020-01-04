c04c7a8461bf42c339680eba4348fe12bc106bae
wind-of <adequation@mail.ru>
2020-01-04 21:26:52 +0300

    redid DTO; added appropriate decorators to properties

--- src/cats/dto/create-cat.dto.ts
+++ src/cats/dto/create-cat.dto.ts
@@ -1,5 +1,18 @@
+import { IsInt, IsString } from 'class-validator'
+
+// export class CreateCatDto {
+//   readonly name: string;
+//   readonly age: number;
+//   readonly breed: string;
+// }
+
 export class CreateCatDto {
+  @IsString()
   readonly name: string;
+
+  @IsInt()
   readonly age: number;
+
+  @IsString()
   readonly breed: string;
 }
