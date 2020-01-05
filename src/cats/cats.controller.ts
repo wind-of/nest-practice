d288e12808fea890728d277e3ab67325756e24cf
wind-of <adequation@mail.ru>
2020-01-05 11:22:26 +0300

    bound the guard to the POST-request

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,8 +1,9 @@
-import { Controller, Get, Post, Param, Body, Put, Delete, UsePipes, ValidationPipe } from '@nestjs/common'
+import { Controller, Get, Post, Param, Body, Put, Delete, UsePipes, ValidationPipe, UseGuards, SetMetadata } from '@nestjs/common'
 import { CatsService } from './cats.service'
 import { ICat } from './interfaces/cat.interface'
 import { CreateCatDto } from './dto/create-cat.dto'
 import { UpdateCatDto } from './dto/update-cat.dto'
+import { SomeGuard } from 'src/guards/some.guard'
 // import { JoiValidationPipe } from 'src/pipes/joi.validation.pipe'
 // import { createCatSchema } from 'src/cats/schemas/joi.cats.schema'
 
@@ -11,13 +12,15 @@ export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
   @Post()
-  // Pipes, similar to exception filters, can be 
+  // Pipes, similar to exception filters and guards, can be 
   // method-scoped, controller-scoped and global-scoped,
   // But the difference between them is that pipes can also be param-scoped (see below)
   // createCat(@Body(ValidationPipe) createCatDto: CreateCatDto)
   
-  // @UsePipes(JoiValidationPipe(createCatSchema))
   @UsePipes(ValidationPipe)
+  // @UsePipes(JoiValidationPipe(createCatSchema))
+  @UseGuards(SomeGuard)
+  @SetMetadata('meta', ['test-meta'])
   createCat(@Body() createCatDto: CreateCatDto) {
     return this.catsService.create(createCatDto);
   }
