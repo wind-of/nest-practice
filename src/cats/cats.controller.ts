1f2f46246d8b0117c25a353761d9fa5c5cb15461
wind-of <adequation@mail.ru>
2020-01-06 15:28:34 +0300

    documentation improvement

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -5,34 +5,67 @@ import { CreateCatDto } from './dto/create-cat.dto'
 import { UpdateCatDto } from './dto/update-cat.dto'
 import { SomeGuard } from 'src/guards/some.guard'
 import { LoggingInterceptor } from 'src/interceptors/logging.interceptor'
-// import { JoiValidationPipe } from 'src/pipes/joi.validation.pipe'
-// import { createCatSchema } from 'src/cats/schemas/joi.cats.schema'
+
+
+/* 
+Order: 
+  Middleware 
+  --> Guards 
+  --> Interceptor
+  --> Pipes
+  --> Controller
+  --> Service
+  --> Interceptor (Outbound)
+  --> Filters
+*/
+
 
 @Controller('cats')
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
-  @Post()
-  // Pipes, similar to exception filters and guards, can be 
-  // method-scoped, controller-scoped and global-scoped,
-  // But the difference between them is that pipes can also be param-scoped (see below)
-  // createCat(@Body(ValidationPipe) createCatDto: CreateCatDto)
+  /* 
+  Pipes, exception filters, guards, interceptor have different scopes:
+  method-scope, controller-scope and global-scope.
+
+  Method-scoped:
+  @Use[Pipes || Filters || Guards || Interceptors]()
+  handler(...) {...}
+
+  Controller-scoped:
+  @Use[Pipes || Filters || Guards || Interceptors]()
+  export class ControllerName{...}
   
-  @UsePipes(ValidationPipe)
-  // @UsePipes(JoiValidationPipe(createCatSchema))
-  @UseInterceptors(LoggingInterceptor)
+  Global-scoped:
+  main.ts:
+    ...
+    const app = await NestFactory.create(AppModule);
+  
+    app.use(MiddlewareName)
+    app.useGlobal[Interceptors || Filters || Pipes || Guards]()
+    ...
+
+  Pipes have an especial scope - param-scope:
+  createCat(@Body(ValidationPipe) createCatDto: CreateCatDto)
+  */
+
+  @Post()
+
   @UseGuards(SomeGuard)
   @SetMetadata('meta', ['test-meta'])
+
   createCat(@Body() createCatDto: CreateCatDto): ICat {
     return this.catsService.create(createCatDto);
   }
 
   @Get()
+  @UseInterceptors(LoggingInterceptor)
   findALl(): ICat[] {
     return this.catsService.findAll();
   }
 
   @Get(':id')
+  @UsePipes(ValidationPipe)
   findOne(@Param() {id}): ICat {
     return this.catsService.findOne(id);
   }
--- src/middlewares/cats.middleware.ts
+++ src/middlewares/cats.middleware.ts
@@ -3,17 +3,19 @@ import { Request, Response } from 'express'
 
 // The CatsMiddleware: Functional and Class Middleware examples
 
-// @Injectable()
-// export class CatsMiddleware implements NestMiddleware{
-//   use(req: Request, res: Response, next: Function) {
-//     console.log('Hi, Console.');
-//     next()
-//   }
-// }
+/*
+@Injectable()
+export class CatsMiddleware implements NestMiddleware{
+  use(req: Request, res: Response, next: Function) {
+    console.log('Hi, Console.');
+    next()
+  }
+}
 
-// Consider using the function middleware (like below), if your middleware doesn't need any dependencies
+Consider using the function middleware (like below), if your middleware doesn't need any dependencies
+*/
 
 export function CatsMiddleware(req: Request, res: Response, next: Function) {
-  console.log('Hi, Console.');
+  console.log('Hi, Console, I\'m Middleware.');
   next()
 }
