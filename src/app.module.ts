aaebb0f78ac79e688dbf5d1de4731fb17ddd16b6
wind-of <adequation@mail.ru>
2020-01-11 15:14:16 +0300

    Cats initialization

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
+import { CatsModule } from './cats/cats.module';
 
 @Module({
   controllers: [AppController],
+  imports: [CatsModule],
 })
 export class AppModule {}
---
+++ src/cats/cats.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+import { CatsService } from './cats.service';
+
+@Controller('cats')
+export class CatsController {
+  constructor(private readonly catsService: CatsService) {}
+
+  @Get()
+  Greet(): string {
+    return "Hello, World!"
+  } 
+}
